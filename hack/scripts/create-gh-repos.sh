eb2d860d60423bf9a59600aeeb986cec81a230c0
Tamal Saha <tamal@appscode.com>
2025-03-07 22:48:58 -0800

    Format scripts
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/create-gh-repos.sh
+++ hack/scripts/create-gh-repos.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-projects=( \
-	virtual-secrets \
+projects=(
+    virtual-secrets
 )
 
 gsa=$(cat /Users/tamal/AppsCode/credentials/appscode-govanityurls.json)
 # gh secret set FIREBASE_SERVICE_ACCOUNT_APPSCODE_GOVANIURLS -b"${gsa}" -R appscodelabs/govanityurls
 
 for p in "${projects[@]}"; do
-	# gh repo create -y --public --template=appscodelabs/govanityurls $p/govanityurls
-	gh secret set FIREBASE_SERVICE_ACCOUNT_APPSCODE_GOVANIURLS -b"${gsa}" -R $p/govanityurls
+    # gh repo create -y --public --template=appscodelabs/govanityurls $p/govanityurls
+    gh secret set FIREBASE_SERVICE_ACCOUNT_APPSCODE_GOVANIURLS -b"${gsa}" -R $p/govanityurls
 done
